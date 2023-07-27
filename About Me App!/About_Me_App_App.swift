7766e950891475c06db360b6d99e2934468b182b
Victoria <victoria.gclcc@gmail.com>
2023-07-27 13:40:06 +0100

    Initial Commit

---
+++ About Me App!/About_Me_App_App.swift
@@ -0,0 +1,17 @@
+//
+//  About_Me_App_App.swift
+//  About Me App!
+//
+//  Created by scholar on 27/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct About_Me_App_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
