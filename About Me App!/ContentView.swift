72b64064891dbb1489fcad329ccecc0b85cec3b2
Victoria <victoria.gclcc@gmail.com>
2023-08-01 09:31:28 +0100

    Added comment

--- About Me App!/ContentView.swift
+++ About Me App!/ContentView.swift
@@ -6,16 +6,32 @@
 //
 
 import SwiftUI
+//COMENT
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(alignment: .center) {
+            Text("About Me")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.pink)
+            Image("Me")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .padding()
+            Text("My Facts")
+                .font(.title)
+                .fontWeight(.bold)
+            Text("I'm really passionate about fashion")
+            Text("I love dressing up")
+            Text("I do Pilates during my free time")
+            Text("I love learning new languages, currently I'm learning TURKISH")
+            Text("There series are AMAZING!!!!")
+                . padding()
+                
+            
         }
-        .padding()
+     
     }
 }
 
